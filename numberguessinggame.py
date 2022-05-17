0c9798f782a5fbdf62005fea5b97dfb5b2ee441c
mattdmartin1 <91096561+mattdmartin1@users.noreply.github.com>
2022-05-16 20:02:52 -0700

    Second commit

--- numberguessinggame.py
+++ numberguessinggame.py
@@ -1,16 +1,20 @@
+# computer has a secret number
+# we are trying to guess that secret number 
+
 import random
 
 def guess(x):
     random_number = random.randint(1, x)
-    guess = 0 
+    guess = 0
     while guess != random_number:
         guess = int(input(f'Guess a number between 1 and {x}: '))
         if guess < random_number:
-            print('Sorry guess again. Too low. ')
+            print('Your guess is too low. Please try again. ')
         elif guess > random_number:
-            print('Sorry guess again. Too high. ')
-    
-    print(f'Yay congrats. You guessed the number {random_number} correctly!! ')
-    
-    
+            print('Your guess is too high. Please try again. ')
+            
+    print(f'You guessed the number {random_number} correctly! Congradulations!!! ')
+
+
+
 guess(10)
