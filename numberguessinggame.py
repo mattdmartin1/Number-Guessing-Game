cd4df4f722c794da611955a9662d7ea3fc1e87f0
mattdmartin1 <91096561+mattdmartin1@users.noreply.github.com>
2022-05-16 20:25:23 -0700

    Third commit

--- numberguessinggame.py
+++ numberguessinggame.py
@@ -16,5 +16,22 @@ def guess(x):
     print(f'You guessed the number {random_number} correctly! Congradulations!!! ')
 
 
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low # could also be high b/c low = high
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)?? ').lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+    
+    print(f'Yay! The computer guessed your number, {guess}, correctly! ')
 
-guess(10)
+
+computer_guess(1000)
